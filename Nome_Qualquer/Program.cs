1f6ddb03ae4f7661345a9135e5ec4b616b7882e8
Tainara Limeira Leal <tainaraleal.if@gmail.com>
2023-10-06 09:30:07 -0400

    primeiro commit

---
+++ Nome_Qualquer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite outro número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num + num2;
+
+        Console.WriteLine($"O resultado da multiplicação é {resultado}");
+
+        Console.ReadKey();
+    }
+}
